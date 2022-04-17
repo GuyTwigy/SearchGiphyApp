8bee508144196d17d2d56be3f0ad34a39037b4b7
guy twig <guy.twig@greenco.tech>
2022-04-18 00:07:43 +0300

    Add new Xcode project

---
+++ SearchGiphyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchGiphyApp
+//
+//  Created by Guy Twig on 18/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
